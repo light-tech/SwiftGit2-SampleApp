6505f299c82a6dc8b60e1514f4c3ec1122274439
Lightech <lightech@outlook.com>
2021-05-24 08:19:00 -0700

    Add SwiftGit2 as a submodule, remove redundant xcframeworks and update setup script as we switch to the combined xcframework

--- setup.sh
+++ setup.sh
@@ -1,11 +1,8 @@
 # Download the prebuilt frameworks
-FRAMEWORKS=(libpcre openssl libssh2 libgit2)
+FRAMEWORKS=(libgit2)
 FRAMEWORKS_RELEASE_URL=https://github.com/light-tech/LibGit2-On-iOS/releases/download/v1.1.0
 for framework in ${FRAMEWORKS[@]}; do
-	wget $FRAMEWORKS_RELEASE_URL/$framework.xcframework.zip
-	unzip $framework.xcframework.zip
+	wget $FRAMEWORKS_RELEASE_URL/$framework.zip
+	unzip $framework.zip
 done
 
-# Copy the source code subdir SwiftGit2 outside
-git clone https://github.com/SwiftGit2/SwiftGit2.git SG2
-cp -r SG2/SwiftGit2 .
